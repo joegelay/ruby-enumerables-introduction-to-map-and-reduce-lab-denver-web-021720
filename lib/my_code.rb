0a3e6149dda3cc1e84b99ac780857e82c1299c35
Joe Gelay <joegelay@gmail.com>
2020-01-24 01:14:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -61,7 +61,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
    while i < source_array.length do 
-   result << (source_array[i] ** 2)
+   return false if !source_array[i]
    i += 1 
   end 
 end
